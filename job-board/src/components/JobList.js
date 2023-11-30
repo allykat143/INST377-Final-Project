fe02b12d78f37648c9874cbb9600f9b755c88c4e
Allyson Clipper <allysonC97@gmail.com>
2023-11-29 21:10:17 -0500

    modified api request results to include the link to job posting

--- job-board/src/components/JobList.js
+++ job-board/src/components/JobList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 const JobList = ({ jobs }) => {
-  //console.log('Jobs:', jobs); 
+  console.log('Jobs:', jobs); 
   return (
     <ul>
       {jobs.map((job) => (
         <li key={job.MatchedObjectId}>
           <strong>{job.MatchedObjectDescriptor.PositionTitle}</strong>
-          <p>{job.MatchedObjectDescriptor.OrganizationName}</p>
+          <p> Company: {job.MatchedObjectDescriptor.OrganizationName}</p>
+          <p>Application Link: {job.MatchedObjectDescriptor.PositionURI}</p>
           <p>Application Close Date: {job.MatchedObjectDescriptor.ApplicationCloseDate}</p>
         </li>
       ))}
