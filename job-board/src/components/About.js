24c6e0f238ac1b39483ca7bf598c1806f92712d0
maritrotz <maritrotz@gmail.com>
2023-11-30 22:19:33 -0500

    Updated the About page

--- job-board/src/components/About.js
+++ job-board/src/components/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import trotzImage from './KTrotz.JPG';
 
 const About = () => {
   return (
     <div>
-      <h2>About Us</h2>
+      <h2 id='aboutUs'>About Us</h2>
       <p>This is a simple job board application designed to display open jobs for minorities.</p>
+      <div class = "img-container">
+        <img src={trotzImage} alt="Binary" width="450" height="580" />
+      </div>
     </div>
   );
 };
