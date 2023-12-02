e986b79ac458e88da9d4b4ced420fdd2fb32a479
Allyson Clipper <allysonC97@gmail.com>
2023-12-01 19:28:02 -0500

    formatted contact page

--- job-board/src/components/Contact.js
+++ job-board/src/components/Contact.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Contact = () => {
     return (
       <div>
-        <h2>Contact Us</h2>
-        <p>This is the contact page</p>
+        <h2 id='contact'>Contact Us</h2>
+        <p id='email'>Email: minorityjobs@gmail.com</p>
+        <p id='phone'>Phone: 888-888-8888</p>
       </div>
     );
   };
